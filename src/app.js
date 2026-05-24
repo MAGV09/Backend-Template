563204bcfe4ef2a991ae78cf18a765a5eee6250e
MAGV09 <mohamedadel2002320@gmail.com>
2026-05-24 11:35:52 +0300

    feat: add prisma session

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('node:path');
 const morgan = require('morgan');
-const pool = require('./config/database');
+const prisma = require('./lib/prisma');
 const dotenv = require('dotenv');
 // const session = require('express-session');
-// const pgSession = require('connect-pg-simple')(session);
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 // const passport = require('./config/passport');
 
 const errorHandler = require('./middleware/errorHandler');
@@ -13,11 +13,11 @@ const indexRouter = require('./routes/index');
 
 dotenv.config({ quiet: true });
 
-// const sessionStore = new pgSession({
-//   pool,
-//   tableName: 'sessions',
-//   createTableIfMissing: true,
-// });
+const sessionStore = new PrismaSessionStore(prisma, {
+  checkPeriod: 12 * 60 * 60 * 1000, 
+  dbRecordIdIsSessionId: true,
+  dbRecordIdFunction: undefined,
+});
 
 const app = express();
 app.set('views', path.join(__dirname, '../views'));
@@ -52,13 +52,19 @@ app.use(errorHandler);
 const port = process.env.APP_PORT || 3000;
 async function start() {
   try {
-    await pool.query('SELECT 1');
+    await prisma.$connect();
     console.log('Connected to DB');
     app.listen(port, () => console.log(`Listening on ${port}`));
   } catch (err) {
     console.error('Failed to connect to DB:', err);
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
 
 start();
