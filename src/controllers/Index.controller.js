368bb5cafb1f98b6f878c655a49d22bc2c15f55b
MAGV09 <mohamedadel2002320@gmail.com>
2026-05-24 18:37:00 +0300

    feat: add multer and raw sql query for prisma

--- src/controllers/Index.controller.js
+++ src/controllers/Index.controller.js
@@ -1,7 +1,11 @@
 async function getHomepage(req, res) {
   res.json({
-    message:'Hello'
-  })
+    message: 'Hello',
+  });
 }
 
-module.exports = { getHomepage };
+async function getCurrentUser(req, res) {
+  const { password, ...currentUser } = req.user;
+  res.json(currentUser);
+}
+module.exports = { getHomepage, getCurrentUser };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const router = Router();
-const { getHomepage } = require('../controllers/Index.controller');
+const { getHomepage,getCurrentUser } = require('../controllers/Index.controller');
 const {requireAuth} = require('../middleware/auth')
 
 router.get('/',requireAuth, getHomepage);
+router.get('/users/me', requireAuth, getCurrentUser);
+
 module.exports = router;
